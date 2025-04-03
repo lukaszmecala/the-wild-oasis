2f4f7018b6df353abe45dd1fd8672f83c54f58dc
lukaszmecala <lukaszfronia99@gmail.com>
2025-04-03 18:50:34 +0200

    created pagination

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,29 +1,43 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate,endDate,numNights,numGuests, totalPrice,status, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate,endDate,numNights,numGuests, totalPrice,status, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
+
+  //Filter
 
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
+  // Sort
   if (sortBy) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
   }
 
-  const { data, error } = await query;
+  //Paginate
+
+  const from = (page - 1) * PAGE_SIZE;
+  const to = from + PAGE_SIZE;
+
+  if (page) {
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not get loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
