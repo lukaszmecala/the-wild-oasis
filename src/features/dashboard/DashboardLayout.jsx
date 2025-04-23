6ab9d091d17d02317fe116a80a00755e83014e61
lukaszmecala <lukaszfronia99@gmail.com>
2025-04-23 19:23:16 +0200

    add dashboard layout and filter components with data fetching hooks

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>test</div>
+      <div>Test 2</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,27 @@
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import Spinner from "../ui/Spinner";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useRecentBook } from "../features/dashboard/useRecentBook";
+import { useRecentStays } from "../features/dashboard/useRecentStays";
 
 function Dashboard() {
+  const { isLoading: loadingBookings, bookings } = useRecentBook();
+  const { isLoading: loadingStays, stays, confirmedStays } = useRecentStays();
+
+  console.log("Bookings", bookings);
+
+  if (loadingBookings || loadingStays) return <Spinner />;
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+      <DashboardLayout />
+    </>
   );
 }
 
